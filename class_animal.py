9ab7dacb50245d32d7623352fa7ccd8ab33e93a7
José Cuacés <josecuaces@gmail.com>
2023-01-25 13:34:28 +0100

    modif class_animal et pseudocode

--- class_animal.py
+++ class_animal.py
@@ -14,25 +14,29 @@ import random
 
 class Animal:
     
-    def __init__(self, nom):
+    def __init__(self, nom, grid):
         self.nom = nom
+        self.grid = grid
         
-    def placer_animal(self, grid):
-        x = random.randint(0, len(grid.grille)-1)
-        y = random.randint(0, len(grid.grille[0])-1)
-        while grid.grille[x][y]!= 0 :
-            x = random.randint(0, len(grid.grille)-1)
-            y = random.randint(0, len(grid.grille[0])-1)
-        grid.grille[x][y] = self.nom
+    def placer_animal(self,):
+        x = random.randint(0, len(self.grid.grille)-1)
+        y = random.randint(0, len(self.grid.grille[0])-1)
+        while self.grid.grille[x][y]!= 0 :
+            x = random.randint(0, len(self.grid.grille)-1)
+            y = random.randint(0, len(self.grid.grille[0])-1)
+        self.grid.grille[x][y] = self.nom
         
+    def mouving_animal(self,):
+      
+      pass
 
 
-thon = Animal("T")
-requin = Animal("S")
+thon = Animal("T", ma_grille)
+requin = Animal("S", ma_grille)
 for animal in range (20):
-  thon.placer_animal(ma_grille)
+  thon.placer_animal()
 for animal in range (10):
-  requin.placer_animal(ma_grille)
+  requin.placer_animal()
 
 
 ma_grille.affiche_grille()
